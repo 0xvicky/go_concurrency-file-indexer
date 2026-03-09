ca28518ae16b7c3a55ff84aa2e4e44070b412619
vicky <tyagivivek528@gmail.com>
2026-03-09 21:04:04 +0530

    added the mutex and hash calculation logic in workers

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
+
+type hashStorage struct {
+	mu  sync.Mutex
+	mpp map[string]string
+}
+
+func (h *hashStorage) addHash(resHash string, filePath string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.mpp[filePath] = resHash
+}
 
 // producer function.
 func scanner(dir string, workQueue chan<- string) {
@@ -28,33 +43,54 @@ func scanner(dir string, workQueue chan<- string) {
 
 }
 
-func worker(workerId int, workQueue <-chan string, wg *sync.WaitGroup) {
+func worker(workerId int, workQueue <-chan string, wg *sync.WaitGroup, hs *hashStorage) {
+
 	defer wg.Done()
 	for filePath := range workQueue {
 		println("Worker id", workerId, "processing", filePath)
+		//read the file content using filepath
+		fileContent, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// println(string(fileContent))
+		//calculate the hash using sha256
+		byteHash := sha256.New()
+		byteHash.Write(fileContent)
+		finalHash := hex.EncodeToString(byteHash.Sum(nil))
+		// fmt.Println(finalHash)
+		hs.addHash(finalHash, filePath)
 	}
 }
 
 func main() {
 	println("Concurrent File Indexer")
+	start := time.Now()
 	dir := "Z:/Code/Golang/concurrent-file-indexer/dummy"
+	hstorage := hashStorage{mpp: make(map[string]string)}
 
-	//map to store result
-	// m := make(map[string]string)
 	// //channel to pass filepath to workers
 	pathChannel := make(chan string)
-	go scanner(dir, pathChannel)
+
 	//create waitGroup
 	var wg sync.WaitGroup
-
 	//spawing workers
-	nWorkers := 3
+	nWorkers := 16
 	for i := 1; i <= nWorkers; i++ {
 		wg.Add(1)
-		go worker(i, pathChannel, &wg)
+		go worker(i, pathChannel, &wg, &hstorage)
 	}
+
+	go scanner(dir, pathChannel)
+
 	wg.Wait()
 
+	elapsed := time.Since(start)
+	log.Printf("Time taken with %d workers:%s", nWorkers, elapsed)
+	//check if map is got updated with 5000 hashes
+	mapLen := len(hstorage.mpp)
+	fmt.Println("Total Files Indexed:", mapLen)
 }
 
 /*
@@ -117,13 +153,13 @@ Program exits
 */
 
 /*
-Filesystem
+Filesystem✅
     ↓
-Directory Scanner
+Directory Scanner ✅
     ↓
-File Path Channel
+File Path Channel ✅
     ↓
-Worker Pool (goroutines)
+Worker Pool (goroutines) ✅
     ↓
 Hash Computation
     ↓
